657c1673eb84bb1dccd17975769f0d1c09e4e90e
Rizki Muh Pasinringi <rizkimuhpasinringi@gmail.com>
2022-06-21 08:23:20 +0800

    initial commits

---
+++ Asset/script.js
@@ -0,0 +1,36 @@
+// ambil data
+let toggle = document.querySelector(".toggle-nav input");
+let ul = document.querySelector("nav ul");
+let header = document.querySelector("header");
+
+let button_kategori_1 = document.querySelector("#button-kategori-1")
+let info_varian_1 = document.querySelector("#info-varian-1")
+let button_kategori_2 = document.querySelector("#button-kategori-2")
+let info_varian_2 = document.querySelector("#info-varian-2")
+let closeInfo_1 = document.querySelector(".close-1")
+let closeInfo_2 = document.querySelector(".close-2")
+
+// navbar responsive
+toggle.addEventListener("click", function() {
+	ul.classList.toggle("muncul")
+	header.classList.toggle("muncul-lagi")
+})
+// end Navbar resonsive
+
+// informasi produk 1
+button_kategori_1.addEventListener("click", function() {
+	info_varian_1.classList.add("scale-variant")
+})
+
+closeInfo_1.addEventListener("click", function() {
+	info_varian_1.classList.remove("scale-variant")
+})
+
+// informasi produk 2
+button_kategori_2.addEventListener("click", function() {
+	info_varian_2.classList.add("scale-variant")
+})
+
+closeInfo_2.addEventListener("click", function() {
+	info_varian_2.classList.remove("scale-variant")
+})
